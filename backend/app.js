9ffc380d4b51cb66ed58abbc8ec19d862037febd
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2024-10-12 14:00:38 +0300

    re

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');  // Added path for serving static files
-const sequelize = require('./config/db'); // Import Sequelize instance
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const path = require('path');  // Added path for serving static files
+    const sequelize = require('./config/db'); // Import Sequelize instance
 
-const professionalRoutes = require('./routes/professionalRoutes');
+    const professionalRoutes = require('./routes/professionalRoutes');
 
-const cors = require('cors');
-const routes = require('./routes/clientRoutes'); // Adjust the path
-// Create an Express app
-const app = express();
+    const cors = require('cors');
+    const clientRoutes = require('./routes/clientRoutes'); // Adjust the path
+    // Create an Express app
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(cors());
+    app.use('/api/professionals', professionalRoutes);
+    app.use('/api', clientRoutes);
+    // Serve static files from the React app's build directory
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Middleware for parsing JSON request bodies
-app.use(bodyParser.json());
-
-app.use(cors());
-app.use('/api/professionals', professionalRoutes);
-
-// Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.use('/api', routes);
-// Handle any other routes and send the React frontend
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
-
-// Start the server and connect to the database
-const PORT = 3001; // Fixed port
-
-// Sync the database models and start the server
-sequelize.sync()
-    .then(() => {
-        console.log('Database synced successfully.');
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('Error syncing the database:', err);
+    // Handle any other routes and send the React frontend
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
     });
 
-// Global error handler
-app.use((err, req, res, next) => {
-    console.error('Global error handler:', err);
-    res.status(500).json({ success: false, message: 'An internal error occurred.' });
-});
+    // Start the server and connect to the database
+    const PORT = 3001; // Fixed port
+
+    // Sync the database models and start the server
+    sequelize.sync()
+        .then(() => {
+            console.log('Database synced successfully.');
+            app.listen(PORT, () => {
+                console.log(`Server is running on port ${PORT}`);
+            });
+        })
+        .catch(err => {
+            console.error('Error syncing the database:', err);
+        });
+
+    // Global error handler
+    app.use((err, req, res, next) => {
+        console.error('Global error handler:', err);
+        res.status(500).json({ success: false, message: 'An internal error occurred.' });
+    });
