c2ac87fa2c286806a5cb1de4be49a87e25079e86
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2024-10-21 00:10:09 +0300

    dd

--- frontend/src/pages/professionals/ExpertMainPage.js
+++ frontend/src/pages/professionals/ExpertMainPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../styles/ExpertMainPage.module.css';
 import LanguageSelectionPopup from '../../components/LanguageSelectionPopup';
