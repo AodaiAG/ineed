a1bea6d764ab4c4c9213d7713db486563ef258b2
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2024-10-21 10:34:29 +0300

    jj

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-// Register the service worker to make the app a PWA
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then((registration) => {
-                console.log('Service Worker registered with scope:', registration.scope);
-            })
-            .catch((error) => {
-                console.error('Service Worker registration failed:', error);
-            });
-    });
-}
+// Register the service worker
+serviceWorkerRegistration.register();
