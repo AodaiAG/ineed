a5eeb9a085bd4dd41f5317a715278d57cf75d71d
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2024-10-25 10:44:26 +0300

    dd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
                     <Route path="/location" element={<LocationPage />} />
                     <Route path="/information" element={<InfoPage />} />
                     <Route path="/thankyou" element={<ThankYouPage />} />
-
+//
                     {/* Professional Side Routes */}
                     <Route path="/pro/enter" element={<ProfessionalPhoneScreen />} />
                     <Route path="/pro/sms-verification" element={<SMSVerification />} />
